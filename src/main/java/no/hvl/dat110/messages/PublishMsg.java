4dc9387423836c7b3bf090c5fcdca982b6b9047f
simenihlen <simenihlen@users.noreply.github.com>
2023-02-18 14:37:17 +0100

    Task A er ferdig.

--- src/main/java/no/hvl/dat110/messages/PublishMsg.java
+++ src/main/java/no/hvl/dat110/messages/PublishMsg.java
@@ -4,12 +4,38 @@ import no.hvl.dat110.common.TODO;
 
 public class PublishMsg extends Message {
 	
-	// message sent from client to create publish a message on a topic 
+	// message sent from client to create publish a message on a topic
+
+	private String topic;
+	private String message;
 
 	public PublishMsg(String user, String topic, String message) {
 
+		super(MessageType.PUBLISH, topic);
+		this.topic = topic;
+		this.message = message;
+
 	}
 
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "PublishMsg{" +
+				"topic='" + topic + '\'' +
+				", message='" + message + '\'' +
+				'}';
+	}
 	// TODO:
 	// Implement object variables - a topic and a message is required
 
@@ -17,7 +43,6 @@ public class PublishMsg extends Message {
 	// as described in the project text
 	
 	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		return message;
 	}
 }
--- src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
@@ -2,13 +2,33 @@ package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
 
-	// message sent from client to subscribe on a topic 
+	// message sent from client to subscribe on a topic
+
+    private String topic;
 
     public SubscribeMsg(String user, String topic) {
 
+        super(MessageType.SUBSCRIBE, user);
+        this.topic = topic;
+
     }
 
-	// TODO:
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscribeMsg{" +
+                "topic='" + topic + '\'' +
+                '}';
+    }
+
+    // TODO:
 	// Implement object variables - a topic is required
 
 	// Complete the constructor, get/set-methods, and toString method
